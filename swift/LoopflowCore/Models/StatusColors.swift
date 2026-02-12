280b61ceda99309ec39248cf1c7052fb218a0589
Jack Heart <jack@loopflow.studio>
2026-02-12 15:56:32 -0800

    compress: deduplicate Color.init(hex:) across modules
    Make the LoopflowCore init public so Concerto inherits it.
    Removes the duplicate definition from BrandColors.swift.

--- swift/LoopflowCore/Models/StatusColors.swift
+++ swift/LoopflowCore/Models/StatusColors.swift
@@ -10,7 +10,7 @@ extension Color {
     public static let statusInfo = Color(hex: 0x0AB3CC)
     public static let statusNeutral = Color(hex: 0x8B8B8B)
 
-    init(hex: UInt, alpha: Double = 1) {
+    public init(hex: UInt, alpha: Double = 1) {
         self.init(
             .sRGB,
             red: Double((hex >> 16) & 0xff) / 255,

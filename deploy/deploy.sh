2f722282317875b7b84ac96b22342733254a070a
Jack Heart <jack@loopflow.studio>
2026-02-14 08:17:24 -0800

    lf commit: deploy: move infra files into deploy/ and docker/ directories

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Deploy lfd stack to a remote host.
 #
-# Usage: ./deploy.sh [host]
+# Usage: deploy/deploy.sh [host]
 #
 # Expects a git clone of this repo on the remote host at ~/loopflow.
 # Pulls the latest code, copies .env, and restarts the stack.
 #
 # First-time setup on the instance:
 #   git clone https://github.com/loopflowstudio/loopflow.git ~/loopflow
-#   cp .env ~/loopflow/.env  # (with LFD_AUTH_TOKEN set)
+#   cp .env ~/loopflow/docker/.env  # (with LFD_AUTH_TOKEN set)
 #
 # Prerequisites:
 #   - SSH access configured (~/.ssh/config entry for the host)
@@ -22,6 +22,6 @@ echo "Deploying to $HOST..."
 
 # Pull latest and restart
 ssh "$HOST" 'cd ~/loopflow && git pull'
-scp .env "$HOST":~/loopflow/.env
-ssh "$HOST" 'cd ~/loopflow && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build'
-ssh "$HOST" 'cd ~/loopflow && docker compose -f docker-compose.yml -f docker-compose.prod.yml ps'
+scp docker/.env "$HOST":~/loopflow/docker/.env
+ssh "$HOST" 'cd ~/loopflow && docker compose -f docker/docker-compose.yml -f deploy/docker-compose.prod.yml up -d --build'
+ssh "$HOST" 'cd ~/loopflow && docker compose -f docker/docker-compose.yml -f deploy/docker-compose.prod.yml ps'

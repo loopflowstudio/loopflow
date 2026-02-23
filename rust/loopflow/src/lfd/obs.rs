1b0c3c42da925c2ed3d2ef685ab63aaf4522c0c6
Jack Heart <jack@loopflow.studio>
2026-02-23 09:15:05 -0800

    lf commit: lfd obs: direct tracing output to stderr

--- rust/loopflow/src/lfd/obs.rs
+++ rust/loopflow/src/lfd/obs.rs
@@ -2,5 +2,8 @@ use tracing_subscriber::{fmt, EnvFilter};
 
 pub fn init_tracing() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    fmt().with_env_filter(filter).init();
+    fmt()
+        .with_writer(std::io::stderr)
+        .with_env_filter(filter)
+        .init();
 }

58183e67730cdee9237eab2087fbd69cc9b5aa7f
Jack Heart <jack@loopflow.studio>
2026-03-17 12:04:17 -0700

    lf commit: compress

--- swift/LoopflowCore/State/RepoState.swift
+++ swift/LoopflowCore/State/RepoState.swift
@@ -246,7 +246,6 @@ public final class RepoState {
         worktreeStore.removeAll()
         sessionStates.removeAll()
         waitingSessionIds.removeAll()
-        attentionStore.removeAll()
         optimisticInteractiveWaveIds.removeAll()
         selectedWaveId = nil
         isLoading = false

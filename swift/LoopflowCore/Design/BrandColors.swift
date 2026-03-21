9baa780675d7584f96fc09767ca9ed1df66b2087
Jack Heart <jack@loopflow.studio>
2026-03-21 16:50:34 -0700

    ci: fix boundary check and concerto-ui-test timeout
    Move NSColor extension from LoopflowCore to Platform/macOS to satisfy
    the multiplatform boundary checker (AppKit import not allowed in shared
    code). Fix concerto-ui-test timeout by terminating xcodebuild before
    checking xcresulttool, so the result bundle lock is released.

--- swift/LoopflowCore/Design/BrandColors.swift
+++ swift/LoopflowCore/Design/BrandColors.swift
@@ -70,16 +70,6 @@ public struct LoopflowPalette: Sendable {
     )
 }
 
-#if canImport(AppKit)
-import AppKit
-
-public extension NSColor {
-    static let loopflowDarkBackground = NSColor(
-        red: 0x2B / 255.0, green: 0x30 / 255.0, blue: 0x36 / 255.0, alpha: 1
-    )
-}
-#endif
-
 public struct PaletteKey: EnvironmentKey {
     public static let defaultValue = LoopflowPalette.light
 }
